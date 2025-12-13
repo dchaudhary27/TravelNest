196548ba6777d860dc927def872c016b4d566966
Dhaval Chaudhary <dhavalkumar.c@ahduni.edu.in>
2025-12-13 01:29:35 -0500

    feat: fixed update and delete function.

--- Model/home.js
+++ Model/home.js
@@ -7,19 +7,35 @@ const rootDir = require("../util/pathutils");
 const { ObjectId } = require("mongodb");
 
 module.exports = class Home {
-  constructor(homeName, rentPerDay, address, rating, photo, description, id) {
+  constructor(homeName, rentPerDay, address, rating, photo, description, _id) {
     this.homeName = homeName;
     this.rentPerDay = rentPerDay;
     this.address = address;
     this.rating = rating;
     this.photo = photo;
     this.description = description;
-    this._id = _id;
+    if (_id) {
+      this._id = _id;
+    }
   }
 
   save() {
     const db = getDb();
-    return db.collection("homes").insertOne(this);
+    const updateHome = {
+      homeName: this.homeName,
+      rentPerDay: this.rentPerDay,
+      address: this.address,
+      rating: this.rating,
+      photo: this.photo,
+      description: this.description,
+    };
+    if (this._id) {
+      return db
+        .collection("homes")
+        .updateOne({ _id: new ObjectId(this._id) }, { $set: updateHome });
+    } else {
+      return db.collection("homes").insertOne(this);
+    }
   }
 
   static fetchHomes() {
@@ -34,5 +50,10 @@ module.exports = class Home {
       .find({ _id: new ObjectId(String(homeID)) })
       .next();
   }
-  static deleteByID(homeID) {}
+  static deleteByID(homeID) {
+    const db = getDb();
+    return db
+      .collection("homes")
+      .deleteOne({ _id: new ObjectId(String(homeID)) });
+  }
 };
