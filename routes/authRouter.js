32583be5b4ed1c96fb8ff29805678a0683160d24
Dhaval Chaudhary <dhavalkumar.c@ahduni.edu.in>
2025-12-23 01:08:15 -0500

    fix(auth): resolve merge conflict in authRouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -9,11 +9,8 @@ authRouter.post("/login", authController.postLogin);
 
 authRouter.post("/logout", authController.postLogout);
 
-<<<<<<< Updated upstream
-=======
 authRouter.get("/signup", authController.getSignup);
 
 authRouter.post("/signup", authController.postSignup);
 
->>>>>>> Stashed changes
 module.exports = authRouter;
