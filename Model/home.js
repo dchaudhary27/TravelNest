259a6f42e3dedb6921c3bdbb67f564af6470efa9
Dhaval Chaudhary <dhavalkumar.c@ahduni.edu.in>
2026-01-17 21:00:28 -0500

    feat(home): update home schema to include structured photo and houseRules fields, and set default rating value

--- Model/home.js
+++ Model/home.js
@@ -4,10 +4,17 @@ const homeSchema = new mongoose.Schema({
   homeName: { type: String, required: true },
   rentPerDay: { type: Number, required: true },
   address: { type: String, required: true },
-  rating: { type: Number, required: true },
-  photo: { type: String },
+  rating: { type: Number, default: 0 },
   description: { type: String },
-  houseRules: { type: String },
+  photo: {
+    url: { type: String },
+    publicId: { type: String },
+  },
+  houseRules: {
+    url: { type: String },
+    publicId: { type: String },
+  },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Home", homeSchema);
