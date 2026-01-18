a33fb3033b3023f26fa71ff5b2fe9bcc3bc13e52
Dhaval Chaudhary <dhavalkumar.c@ahduni.edu.in>
2026-01-17 21:06:58 -0500

    fix(app): set default port to 3000 if PORT is not defined

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ mongoose
   .connect(MONGO_URL)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
+    app.listen(PORT || 3000, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })
